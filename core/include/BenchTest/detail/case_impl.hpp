91408fb4609f62efe53a0b2de48bc64383add1bd
Mashley22 <ajkhsu22@gmail.com>
2025-10-16 23:17:27 +0100

    Renaming common to core cos that just makes much more sense Also added header for the case_impl class

---
+++ core/include/BenchTest/detail/case_impl.hpp
@@ -0,0 +1,38 @@
+#ifndef BENCHTEST_DETAIL_CASE_IMPL_HPP
+#define BENCHTEST_DETAIL_CASE_IMPL_HPP
+
+#include <string_view>
+
+#include <BenchTest/types.hpp>
+
+namespace benchtest {
+
+namespace detail {
+
+/**@brief The Case encapsulates the core of the methadology
+ *        This contains just some of the components of the thing that are present in all cases
+ */
+class Case {
+public:
+
+/**@brief Constructor for the guy 
+ * @param func the actual function to run
+ * @param name the name of this case
+ */
+  Case(func_t func, std::string_view name);
+
+/**@brief Don't need this, it should all be compile time anyways
+ */
+  Case() = delete;
+
+/**@note for some of the bench marking runs with a bit more stuff */
+protected:
+  func_t m_func;
+  std::string_view m_name;
+};
+
+}
+
+}
+
+#endif /* BENCHTEST_DETAIL_CASE_IMPL_HPP */
